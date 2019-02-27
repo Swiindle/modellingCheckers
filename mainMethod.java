b8a033809aefe6c8863f3c1507bfaa03160f0f9a
Mark <18markl@dyn-10-32-186-46.winet.local>
2019-02-27 15:30:14 +0000

    Added the main method and the gameWindow classes

---
+++ mainMethod.java
@@ -0,0 +1,10 @@
+public class mainMethod
+{
+    public static void main(String[] arguments)
+    {
+        // initialization
+        gameWindow window = new gameWindow(500,500);
+        // function
+        window.open();
+    }
+}
