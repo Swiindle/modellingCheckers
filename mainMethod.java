1fb032547042373ac912cf2a86dcba53ecb38940
Mark <18markl@dyn-v-10-34-199-118.resnet.local>
2019-03-07 23:12:21 +0000

    accidentally removed mainMethod

---
+++ mainMethod.java
@@ -0,0 +1,10 @@
+public class mainMethod
+{
+    public static void main(String[] arguments)
+    {
+        // initialization
+        board b = new board(640,640);
+        // function
+        b.open();
+    }
+}
